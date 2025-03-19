f1a94da609f76be44f1466e2d49ab7352a1d47f1
cnwajiobi <CNwajiobi@Coronationmb.com>
2025-03-19 15:13:15 +0100

    update deposit account controller

--- src/main/java/com/abs/transactionManagement/depositaccount/DepositController.java
+++ src/main/java/com/abs/transactionManagement/depositaccount/DepositController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/deposits")
+@RequestMapping("/deposit-accounts")
 public class DepositController {
     private final DepositService depositService;
 
